81bc6a179335cf6190f85be15f8d03067d6c4e33
pyvnoaim <pyvnovlr@gmail.com>
2025-12-08 14:57:35 +0100

    added page not found and tools

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-6 text-center">
+      <h2 className="text-3xl font-semibold">Page Not Found</h2>
+      <p className="">The resource you’re looking for doesn’t exist</p>
+    </div>
+  )
+}
--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -6,12 +6,7 @@ export default function Peripherals() {
       <main className="flex flex-1 items-center justify-center">
         <div className="flex items-center text-4xl font-bold text-white">
           <span>@</span>
-          <TextType
-            text={['kovaaks', 'crosshairs', 'themes', 'playlists', 'sounds']}
-            cursorCharacter="_"
-            typingSpeed={75}
-            deletingSpeed={75}
-          />
+          <TextType text={['tools']} cursorCharacter="_" typingSpeed={75} deletingSpeed={75} />
         </div>
       </main>
     </div>
