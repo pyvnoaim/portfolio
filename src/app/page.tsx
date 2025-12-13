bc283b2b9e5f31146df97db4dbd0dab509ea0de7
pyvnoaim <pyvnovlr@gmail.com>
2025-12-13 21:12:09 +0100

    removed selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <main className="flex flex-col items-center justify-center gap-6 sm:gap-8">
-        <div className="grid grid-cols-1 gap-4 select-none sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {items.map((item) => (
             <div
               key={item.id}
